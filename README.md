a84f99d49afbe423cd4b680dd55abfac3f4a43e3
Aarsh <aarsh937@gmail.com>
2021-05-01 01:59:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Root Challenge
+
+A c++ program that parses driver trips and returns trip details (see sample i/o for more [details](https://gist.github.com/dan-manges/1e1854d0704cb9132b74)).
+
+# How to Run Locally
+In order to run the c++ program, you must first compile `main.cpp` (it is important to note that each machine compiles c++ differently, so you must recompile once you clone).
+For compilation you can use [clang](https://clang.llvm.org/) or [gcc](https://gcc.gnu.org/), in this case I will show compilation with gcc. Once you have gcc installed, in the root directoy of the repo run,
+```
+g++ -std=c++17 main.cpp -o run
+```
+To pass input to `run` you must use stdin,
+```
+cat INPUT_FILE | ./run 
+```
+or
+```
+./run < INPUT_FILE
+```
+For example,
+
+![alt text](https://i.imgur.com/6sosl5C.png)
+
+## Testing
+The tests can be found under the `test/` directory. Note that the testing is done by bash script `runSuite`, where it takes two arguments, a file with names for all the tests, and the compiled program. The file with the names of all the tests contains the base name of the tests, and within that directory there will exist two files with that base name, one with the `.in` extension (reporesenting input), and another with `.out` (representing expected output) (note both are required!),
+
+In this case we have the file with the test names to be `test/suite.txt`, containing tests like basic_test, where `test/basic_test.in` is the input for the program and `test/basic_test.out` is the expected output.
+
+So to run tests,
+```
+cd test
+./runSuite suite.txt ../run
+```
+
+For example,
+![alt text](https://i.imgur.com/stoLcRB.png)
+
+## How the Program Works
+The program first reads each line of input. If a driver command is seen, it gets added to a map (key value pair data structure), where the key is the driver's name, and the value is the struct Driver. If the trip command is seen, we find the driver in the map, and add the trip information to the Driver struct. Once we are done reading input, we itterate through the map and move each Driver into a priority queue, where the key is the total miles traveled, and the value is the Driver struct. Finally, we itterate through the priority queue from the top, and print out the driver's detail.
+
+## Features and Optimizations
+- Uses smart pointers, which ensures that no memmory is being leaked  (unique_ptrs)
+- Uses best practices for Object oriented programming (data abstraction)
+- Uses map key-value pair data structure to store drivers, so searching for a driver is efficient (search only takes $$O(\log(n))$$ time, where $$n$$ is the number of distinct drivers)
+- Priority queue sorts when inserting, and since we're inserting $$n$$ drivers, sorting only take $$O(n)$$ time, which is much better than conventional sorting $$O(n \log(n))$$, where $$n$$ is number of distinct drivers
+- Overall program is very efficient, given $$n$$ distinct drivers, total run time would be $$O(\log(n)) + O(n) = O(n) \implies \text{linear run time.}$$
+
+## Notes and Important Assumptions for Correctness
+
+- Non stable sort used, so users who traveled the same distance might not be sorted in original order (i.e see test `test/no_move.*`)
+- Last line is a new line for output (i.e see last lines in`test/*.out`), or else tests will fail
+- If a driver that already exists is reintialized (i.e two Driver commands with the same name), nothing happens, the previous driver remains as is (i.e see `test/duplicate_driver.*`)
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+//----------------------------------------------------//
+// Driver struct for users                            //
+//----------------------------------------------------//
+struct Driver
+{
+private:
+    string name;
+    double milesDriven;
+    double totalDuration;
+
+public:
+    Driver(string name)
+        : name(name), milesDriven(0), totalDuration(0) {}
+
+    string getName()
+    {
+        return name;
+    }
+    double getTotalDuration()
+    {
+        return totalDuration;
+    }
+    double getMilesDriven()
+    {
+        return milesDriven;
+    }
+
+    void setMilesDriven(double newMilesDriven)
+    {
+        milesDriven = newMilesDriven;
+    }
+    void setTotalDuration(double newTotalDuration)
+    {
+        totalDuration = newTotalDuration;
+    }
+};
+
+//---------------------------------------------------------------//
+// splits the words in string text and stores it in vector words //
+//---------------------------------------------------------------//
+void splitText(vector<string> *words, string text)
+{
+    istringstream iss(text, istringstream::in);
+    string word;
+    while (iss >> word)
+    {
+        words->push_back(word);
+    }
+}
+
+//---------------------------------------------------------------------//
+// Takes two 24 hr times and calculates the duration from start to end //
+//---------------------------------------------------------------------//
+double getDurationInHours(string start, string end)
+{
+    double NUM_MINUTES_IN_HOUR = 60.0;
+    double startHour = stof(start.substr(0, 2));
+    double startMinutes = stof(start.substr(3, 5));
+
+    double endHour = stof(end.substr(0, 2));
+    double endMinutes = stof(end.substr(3, 5));
+
+    return ((endHour - startHour) * NUM_MINUTES_IN_HOUR + endMinutes - startMinutes) / NUM_MINUTES_IN_HOUR;
+}
+
+//----------------------------------------------------//
+// main                                               //
+//----------------------------------------------------//
+int main()
+{
+    const double MIN_SPEED = 5;
+    const double MAX_SPEED = 100;
+    map<string, unique_ptr<Driver> > drivers; // use key value pair (kvp) to keep track of drivers where key is name, and value is Driver (struct)
+    string text;
+    while (getline(cin, text)) // read stdin
+    {
+        vector<string> words;
+
+        splitText(&words, text);
+
+        if (words[0] == "Driver") // insert new kvp
+        {
+            drivers.insert({words[1], make_unique<Driver>(words[1])});
+        }
+        else // insert trip for existing kvp
+        {
+            Driver *d = drivers.at(words[1]).get();
+            const double tripDuration = getDurationInHours(words[2], words[3]);
+            const double tripSpeed = stof(words[4]) / tripDuration;
+            if (tripSpeed >= MIN_SPEED && tripSpeed <= MAX_SPEED)
+            {
+                d->setMilesDriven(d->getMilesDriven() + stof(words[4]));
+                d->setTotalDuration(d->getTotalDuration() + tripDuration);
+            }
+        }
+    }
+
+    priority_queue<pair<double, unique_ptr<Driver> > > pq; // we use max heap to sort, where key is miles driven, and values is Driver (struct)
+
+    for (auto &it : drivers) // itterate through map kvp
+    {
+        pq.push(make_pair(it.second->getMilesDriven(), move(it.second))); // insert to max heap
+    }
+
+    while (!pq.empty()) // itterate through priority queue kvp and print drivers
+    {
+        Driver *d = pq.top().second.get();
+        if ((d->getMilesDriven()) == 0 || (d->getTotalDuration()) == 0)
+        {
+            cout << d->getName() << ": 0 miles" << endl;
+        }
+        else
+        {
+            const double averageSpeed = d->getMilesDriven() / d->getTotalDuration();
+            cout << d->getName() << ": " << round(d->getMilesDriven()) << " miles @ " << round(averageSpeed) << " mph" << endl;
+        }
+        pq.pop();
+    }
+}
